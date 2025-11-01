397e68ac4fa1feda66a8b112da0df7aa2738e60f
Full-Stack-Sanctus <Full-Stack-Sanctus@gmail.com>
2025-11-01 07:58:34 +0000

    Pushing commit

---
+++ apps/mobile/datingapp-mobile/babel.config.js
@@ -0,0 +1,21 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+export default function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        require.resolve('babel-plugin-module-resolver'),
+        {
+          root: ['./'],
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+          alias: {
+            'shared': '../../packages/shared',
+          },
+        },
+      ],
+    ],
+  };
+}
--- packages/shared/index.tsx
+++ packages/shared/index.tsx
@@ -1,2 +1,5 @@
 // packages/shared/index.ts
 export { HomeScreen } from './components/HomeScreen';
+
+
+//export * from './components';
