c2628687aee55e6226bc70783d37dc2339545b77
Full-Stack-Sanctus <Full-Stack-Sanctus@gmail.com>
2025-11-11 11:09:57 +0000

    Pushing commit

--- packages/shared/index.tsx
+++ packages/shared/index.tsx
@@ -1,5 +1,6 @@
 // packages/shared/index.ts
-export { HomeScreen } from './components/HomeScreen';
-
+export { default as HomeScreen } from './components/HomeScreen';
+// export { default as ProfileCard } from './components/ProfileCard';
+// export { default as CustomButton } from './components/CustomButton';
 
 //export * from './components';
